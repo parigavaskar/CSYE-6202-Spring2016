61ef52c5436a6226c4e27d6114659d0dcf37706d
Parima Gavaskar <parigavaskar@gmail.com>
2016-02-14 18:45:34 -0500

    final commit.........I submitted one before deadline too but this is the one buld on the sln likeyou explained the other day after lab

--- FizzBuzz/FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -1,14 +1,33 @@
-﻿namespace FizzBuzz
-{
-	public class FizzBuzz
-	{   
-		public string RunFizzBuzz(int number)
-		{
-			string result = number.ToString();
+﻿using System;
 
-           //
+namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public string RunFizzBuzz(int number)
+        {
+            string result = number.ToString();
+            if (number == 0)
+            {
+                result ="0";
+            }
+           else if (number % 3 == 0 && number % 5 == 0)
+            {
+                result = "FizzBuzz";
+            }
+           else if (number % 5 == 0)
+            {
+                result = "Buzz";
+            }
+
+           else if (number % 3 == 0)
+            {
+                result = "Fizz";
+            }
+            else result = number.ToString();
 
             return result;
-		}
-	}
+        }
+
+    }
 }
--- FizzBuzz/FizzBuzz/Program.cs
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace FizzBuzz
+﻿using System;
+namespace FizzBuzz
+
 {
 	class Program
 	{
 		static void Main(string[] args)
-		{
+		{   
+            FizzBuzz f = new FizzBuzz();
+            System.Console.WriteLine("Please enter a number:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            string answer= f.RunFizzBuzz(n);
+            Console.WriteLine(answer);
+            Console.ReadLine();
+            
 		}
 	}
 }
