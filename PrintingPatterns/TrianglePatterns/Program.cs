ba6b4ffb4973ec4a4813fb5b26702bc9a91818d2
Parima Gavaskar <parigavaskar@gmail.com>
2016-02-15 14:44:02 -0500

    final commit

--- PrintingPatterns/TrianglePatterns/Program.cs
+++ PrintingPatterns/TrianglePatterns/Program.cs
@@ -17,21 +17,63 @@ namespace TrianglePatterns
 		static void DisplayPatternA()
 		{
 			// your implementation here
-		}
+            for(int i=1; i<=10; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                { Console.Write("*"); }
 
-		static void DisplayPatternB()
+                Console.WriteLine("");
+            }
+            
+
+        }
+
+        static void DisplayPatternB()
 		{
 			// your implementation here
+            for(int i=10; i>=1; i--)
+            {
+                for(int j=1; j<= i; j++)
+                {
+                    Console.Write("*");
+                    
+                }
+                Console.WriteLine("");
+            }
 		}
 
 		static void DisplayPatternC()
 		{
 			// your implementation here
-		}
+            for(int i=10; i>=1; i--)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+                for(int a=10;a>=i; a--)
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
 
 		static void DisplayPatternD()
 		{
 			// your implementation here
+            for(int i=1; i<=10; i++)
+            {
+                for(int j=1; j<=10-i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for(int a=1;a<=i; a++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+
+            }
 		}
 	}
 }
